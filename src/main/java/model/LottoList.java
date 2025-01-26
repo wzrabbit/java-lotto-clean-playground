04b4d8bb1ab3f192c72545eab1a545435817a8b1
wzrabbit <curious.wzrabbit@gmail.com>
2025-01-27 00:06:59 +0900

    ✨ feat: 로또 리스트 클래스에 당첨 로또 정보를 이용해 당첨 결과를 계산하는 기능을 추가

--- src/main/java/model/LottoList.java
+++ src/main/java/model/LottoList.java
@@ -1,15 +1,12 @@
 package model;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class LottoList {
     List<Lotto> lottoList;
 
     public LottoList(List<Lotto> lottoList) {
-        this.lottoList = new ArrayList<Lotto>(lottoList);
+        this.lottoList = new ArrayList<>(lottoList);
     }
 
     public List<Lotto> getValue() {
@@ -18,5 +15,42 @@ public class LottoList {
 
     public int size() {
         return lottoList.size();
+    }
+
+    public LottoPrizeResult getLottoResultByWinningLottoInfo(WinningLottoInfo winningLottoInfo) {
+        List<Integer> lottoPrizesCount = new ArrayList<>();
+
+        for (int i = 1; i <= LottoPrizeConstants.PRIZE_TYPE_COUNT.getValue(); i++) {
+            lottoPrizesCount.add(0);
+        }
+
+        lottoList.forEach(lotto -> {
+            int lottoPrizeIndex = getLottoPrizeIndex(lotto, winningLottoInfo);
+            lottoPrizesCount.set(lottoPrizeIndex, lottoPrizesCount.get(lottoPrizeIndex) + 1);
+        });
+
+
+        return new LottoPrizeResult(lottoPrizesCount);
+    }
+
+    int getLottoPrizeIndex(Lotto lotto, WinningLottoInfo winningLottoInfo) {
+        Set<Integer> lottoNumbers = new HashSet<>(lotto.getValue());
+
+        int matchCount = (int) winningLottoInfo.getLottoValue().stream().filter(lottoNumbers::contains).count();
+        boolean isBonusNumberMatched = lottoNumbers.contains(winningLottoInfo.getBonusNumberValue());
+
+        if (matchCount == 6) {
+            return 0;
+        }
+
+        if (matchCount == 5 && isBonusNumberMatched) {
+            return 1;
+        }
+
+        if (matchCount < 3) {
+            return 5;
+        }
+
+        return 7 - matchCount;
     }
 }
