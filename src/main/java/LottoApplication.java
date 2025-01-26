05825ed979c6d2b49cd80fae1cb61f94f92791d7
wzrabbit <curious.wzrabbit@gmail.com>
2025-01-26 17:58:26 +0900

    ✨ feat: 로또 게임의 엔트리 클래스를 구현

---
+++ src/main/java/LottoApplication.java
@@ -0,0 +1,8 @@
+import controller.LottoGameController;
+
+public class LottoApplication {
+    public static void main(String[] args) {
+        LottoGameController lottoGameController = new LottoGameController();
+        lottoGameController.run();
+    }
+}
