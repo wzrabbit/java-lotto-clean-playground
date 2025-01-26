49db09c9fe84760eb8c1a52e8550be6823d203b0
wzrabbit <curious.wzrabbit@gmail.com>
2025-01-27 02:44:03 +0900

    ✨ feat: 수동 구매 기능을 추가

--- src/main/java/controller/LottoGameController.java
+++ src/main/java/controller/LottoGameController.java
@@ -4,45 +4,94 @@ import model.*;
 import view.InputView;
 import view.OutputView;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.IntStream;
 import java.util.stream.Collectors;
 
 public class LottoGameController {
     private final LottoRandomNumberGenerator lottoRandomNumberGenerator = new LottoRandomNumberGenerator();
     private final LottoGenerator lottoGenerator = new LottoGenerator(lottoRandomNumberGenerator);
+    private LottoPurchasePrice lottoPurchasePrice;
+    private int lottoCount;
+    private int manualLottoCount;
+    private LottoList manualLottoList;
     private LottoList lottoList;
     private WinningLottoInfo winningLottoInfo;
 
     public void run() {
         inputLottoPurchasePrice();
+        inputManualLottoCount();
+        inputManualLottoList();
+        makeLottoList(lottoCount);
         inputWinningLottoInfo();
+        printLottoPrizeResult();
     }
 
     private void inputLottoPurchasePrice() {
-        try {
-            final LottoPurchasePrice lottoPurchasePrice = InputView.inputLottoPurchasePrice();
-            makeLottoList(lottoPurchasePrice.getLottoCount());
-        } catch (Exception exception) {
-            OutputView.printErrorMessage(exception.getMessage());
+        while (true) {
+            try {
+                lottoPurchasePrice = InputView.inputLottoPurchasePrice();
+                lottoCount = lottoPurchasePrice.getLottoCount();
+                break;
+            } catch (Exception exception) {
+                OutputView.printErrorMessage(exception.getMessage());
+            }
+        }
+    }
+
+    private void inputManualLottoCount() {
+        while (true) {
+            try {
+                int manualLottoCount = InputView.inputManualLottoCount();
+                if (manualLottoCount > lottoPurchasePrice.getLottoCount()) {
+                    throw new IllegalArgumentException("수동 로또의 개수가 전체 구매할 수 있는 로또 수인 "
+                            + lottoPurchasePrice.getLottoCount()
+                            + "개보다 많아서는 안 됩니다. 수동 로또의 개수를 줄여보세요.");
+                }
+                this.manualLottoCount = manualLottoCount;
+                break;
+            } catch (Exception exception) {
+                OutputView.printErrorMessage(exception.getMessage());
+            }
+        }
+    }
+
+    private void inputManualLottoList() {
+        while (true) {
+            try {
+                manualLottoList = InputView.inputManualLottoList(manualLottoCount);
+                break;
+            } catch (Exception exception) {
+                OutputView.printErrorMessage(exception.getMessage());
+            }
         }
     }
 
     private void makeLottoList(int lottoCount) {
-        lottoList = new LottoList(IntStream.range(0, lottoCount)
+        int autoLottoCount = lottoCount - this.manualLottoList.size();
+        List<Lotto> manualLottoList = this.manualLottoList.getValue();
+        List<Lotto> autoLottoList = IntStream.range(0, autoLottoCount)
                 .mapToObj(i -> lottoGenerator.generate())
-                .collect(Collectors.toList()));
+                .collect(Collectors.toList());
+        List<Lotto> concatedLottoList = new ArrayList<>();
+        concatedLottoList.addAll(manualLottoList);
+        concatedLottoList.addAll(autoLottoList);
 
+        lottoList = new LottoList(concatedLottoList);
         OutputView.printLottoPurchases(lottoList);
     }
 
     private void inputWinningLottoInfo() {
-        try {
-            final Lotto winningLotto = InputView.inputWinningLotto();
-            final BonusNumber bonusNumber = InputView.inputBonusNumber();
-            this.winningLottoInfo = new WinningLottoInfo(winningLotto, bonusNumber);
-            printLottoPrizeResult();
-        } catch (Exception exception) {
-            OutputView.printErrorMessage(exception.getMessage());
+        while (true) {
+            try {
+                final Lotto winningLotto = InputView.inputWinningLotto();
+                final BonusNumber bonusNumber = InputView.inputBonusNumber();
+                this.winningLottoInfo = new WinningLottoInfo(winningLotto, bonusNumber);
+                break;
+            } catch (Exception exception) {
+                OutputView.printErrorMessage(exception.getMessage());
+            }
         }
     }
 
--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -2,8 +2,10 @@ package view;
 
 import model.BonusNumber;
 import model.Lotto;
+import model.LottoList;
 import model.LottoPurchasePrice;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
@@ -18,6 +20,30 @@ public class InputView {
         scanner.nextLine();
 
         return lottoPurchasePrice;
+    }
+
+    public static int inputManualLottoCount() {
+        System.out.println("\n수동으로 구매할 로또 수를 입력해 주세요.");
+        int manualLottoCount = scanner.nextInt();
+        scanner.nextLine();
+
+        return manualLottoCount;
+    }
+
+    public static LottoList inputManualLottoList(int manualLottoCount) {
+        List<Lotto> manualLottoList = new ArrayList<>();
+
+        System.out.println("\n수동으로 구매할 번호를 입력해 주세요.");
+
+        for (int i = 1; i <= manualLottoCount; i++) {
+            Lotto manualLotto = new Lotto(Arrays.stream(scanner.nextLine().split(","))
+                    .map(String::trim)
+                    .map(Integer::parseInt)
+                    .collect(Collectors.toList()));
+            manualLottoList.add(manualLotto);
+        }
+
+        return new LottoList(manualLottoList);
     }
 
     public static Lotto inputWinningLotto() {
