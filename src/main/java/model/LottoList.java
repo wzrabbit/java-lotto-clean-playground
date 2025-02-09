e7b158295e8e4f9e6897b246165340a8189dad0f
wzrabbit <curious.wzrabbit@gmail.com>
2025-02-09 21:30:17 +0900

    ♻️ refactor: import해야 하는 라이브러리들만 import하도록 변경

--- src/main/java/model/LottoList.java
+++ src/main/java/model/LottoList.java
@@ -1,6 +1,9 @@
 package model;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class LottoList {
     List<Lotto> lottoList;
